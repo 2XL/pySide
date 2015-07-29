ee494d1e64123ee75989758da03a09cfa1f86f7c
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-07-29 23:30:19 +0200

    Interficie gruya amb load bar & menu bar, to lear button, statusbar and event based state machine...

--- BB/main.py
+++ BB/main.py
@@ -7,7 +7,7 @@ import sys
 import time
 from PySide.QtCore import Qt,  QDateTime, QTimer, SIGNAL
 from PySide.QtGui import QApplication, QLabel, QWidget, QIcon, QToolTip, QPushButton, QMessageBox, QDesktopWidget, \
-    QLCDNumber, QMainWindow, QStatusBar, QProgressBar
+    QLCDNumber, QMainWindow, QStatusBar, QProgressBar, QTextEdit, QAction, QKeySequence
 
 
 class MainWindow(QMainWindow):
@@ -17,7 +17,8 @@ class MainWindow(QMainWindow):
         """ Constructor Fucntion
         """
         QMainWindow.__init__(self)
-        self.setWindowTitle("Main Window")
+        self.setWindowTitle("BenchBox GUI v0.0.1")
+        self.setWindowIcon(QIcon('icon/face.png'))
         self.setGeometry(300, 250, 400, 300)
         self.statusLabel = QLabel('Showing Progress')
         self.progressBar = QProgressBar()
@@ -38,9 +39,65 @@ class MainWindow(QMainWindow):
         """ Function to show progress
         """
         while(self.progressBar.value() < self.progressBar.maximum()):
-            self.progressBar.setValue(self.progressBar.value() + 10)
-            time.sleep(1)
+            self.progressBar.setValue(self.progressBar.value() + 45)
+            time.sleep(1) # no sleep
             self.statusLabel.setText('Ready')
+
+    def SetupComponents(self):
+        """ Setting the central widget
+        """
+        self.myStatusBar = QStatusBar()
+        self.setStatusBar(self.myStatusBar)
+        self.myStatusBar.showMessage('Ready', 10000)
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.CreateActions()
+        self.CreateMenus()
+        self.fileMenu.addAction(self.newAction)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAction)
+        self.editMenu.addAction(self.copyAction)
+        self.fileMenu.addSeparator()
+        self.editMenu.addAction(self.pasteAction)
+        self.helpMenu.addAction(self.aboutAction)
+
+    def newFile(self):
+        self.textEdit.setText('')
+    def exitFile(self):
+        self.close()
+    def aboutHelp(self):
+        QMessageBox.about(self, "About Simple Text Editor",
+                          "This example demonstrates the use "
+                          "of Menu Bar")
+    def CreateActions(self):
+        """ Function to create actions for menus
+        """
+        self.newAction = QAction( QIcon('new.png'), '&New',
+                              self, shortcut=QKeySequence.New,
+                              statusTip="Create a New File",
+                              triggered=self.newFile)
+        self.exitAction = QAction( QIcon('exit.png'), 'E&xit',
+                           self, shortcut="Ctrl+Q",
+                           statusTip="Exit the Application",
+                           triggered=self.exitFile)
+        self.copyAction = QAction( QIcon('copy.png'), 'C&opy',
+                           self, shortcut="Ctrl+C",
+                           statusTip="Copy",
+                           triggered=self.textEdit.copy)
+        self.pasteAction = QAction( QIcon('paste.png'), '&Paste',
+                            self, shortcut="Ctrl+V",
+                            statusTip="Paste",
+                            triggered=self.textEdit.paste)
+        self.aboutAction = QAction( QIcon('about.png'), 'A&bout',
+                            self, statusTip="Displays info about text editor",
+                            triggered=self.aboutHelp)
+        # Actual menu bar item creation
+    def CreateMenus(self):
+        """ Function to create actual menu bar
+        """
+        self.fileMenu = self.menuBar().addMenu("&File")
+        self.editMenu = self.menuBar().addMenu("&Edit")
+        self.helpMenu = self.menuBar().addMenu("&Help")
 
 
 class SampleWindow(QWidget):
@@ -159,6 +216,7 @@ if __name__ == '__main__':
         """
         mainWindow = MainWindow()
         mainWindow.CreateStatusBar()
+        mainWindow.SetupComponents()
         mainWindow.show()
         mainWindow.ShowProgress()
         myApp.exec_()
